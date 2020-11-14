2ab7a860ee03033782d5b80b4af20090f665608f
Michael <michel.wojciech@gmail.com>
2020-11-14 19:26:07 +0100

    server running file

--- nodejs-express-sequelize-postgresql/server.js
+++ nodejs-express-sequelize-postgresql/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -18,9 +18,9 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to michal application."});
 });
 
-const PORT = proces.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 
